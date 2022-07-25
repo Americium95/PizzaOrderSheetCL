4bdac97469fa5e4c560b8c0e6599f56bcd2b1fa6
Americium <sunk0525@naver.com>
2022-07-25 22:11:42 +0900

    add:플레이어 구분자 추가

--- Assets/Script/NetWork/networkTransform.cs
+++ Assets/Script/NetWork/networkTransform.cs
@@ -23,6 +23,7 @@ public class networkTransform : MonoBehaviour
     {
         dt += Time.deltaTime;
         transform.position = new Vector3(Mathf.Cos(Time.time) * 10, Mathf.Sin(Time.time) * 10, 10);
+        transform.Rotate(new Vector3(0.25f, 0.5f, 1f));
 
         //갱신주기
         if (dt > UpdateTime)
@@ -31,6 +32,9 @@ public class networkTransform : MonoBehaviour
 
             //transform정보 페킷 구성
             List<byte> packet = new List<byte> { 0x01, 0x00 };
+            //유저넘버 구성
+            packet.Add(0x00);
+            packet.Add(0x01);
             //위치정보 구성
             packet.AddRange(System.BitConverter.GetBytes(transform.position.x));
             packet.AddRange(System.BitConverter.GetBytes(transform.position.y));
@@ -42,18 +46,22 @@ public class networkTransform : MonoBehaviour
 
 
             //ui디버깅용
-            Console.text =
+            Console.text = 
+                (
                 "\n" +
                 string.Join(' ', packet) +
                 "\n" +
-                "Px" + System.BitConverter.ToSingle(packet.ToArray(), 2) +
-                "Py" + System.BitConverter.ToSingle(packet.ToArray(), 6) +
-                "Pz" + System.BitConverter.ToSingle(packet.ToArray(), 10)+
+                "Px" + System.BitConverter.ToSingle(packet.ToArray(), 4) +
+                "Py" + System.BitConverter.ToSingle(packet.ToArray(), 8) +
+                "Pz" + System.BitConverter.ToSingle(packet.ToArray(), 12)+
                 "\n" +
-                "Rx" + packet[14] * 1.4f +
-                "Ry" + packet[15] * 1.4f +
-                "Rz" + packet[16] * 1.4f +
-                Console.text;
+                "Rx" + packet[16] * 1.4f +
+                "Ry" + packet[17] * 1.4f +
+                "Rz" + packet[18] * 1.4f +
+                Console.text
+                );
+
+            Console.text = Console.text.Substring(0, Mathf.Min(Console.text.Length, 500) );
 
             /*Debug.Log(
                 string.Join(' ', packet)+
@@ -64,6 +72,8 @@ public class networkTransform : MonoBehaviour
             );*/
 
             //NetWork.bootstrapChannel.WriteAndFlushAsync( Encoding.UTF8.GetString(packet.ToArray()) );
+
+
 
             //전송
             NetWork.send(packet);
